3bbca49eaf29666d55a0d0ff39807782e26e940c
cedricbai <cedriccan48@gmail.com>
2019-05-25 08:34:51 +0800

    sf

--- src/App.js
+++ src/App.js
@@ -187,7 +187,7 @@ export class MapContainer extends Component {
 
          var the_temperature = "\nTemperature: " + data.currently.apparentTemperature + " F";
 
-         var the_humidity = "\nHumidity: " + data.currently.humidity + "%"
+         var the_humidity = "\nHumidity: " + data.currently.humidity * 100 + "%"
 
           var the_windspeed = "\nWindspeed: " + data.currently.windSpeed + "mph";
 
